68db510c5fb6a5f6349685e2edbe211c111411d0
ilfm <dnehd1828@gmail.com>
2022-04-26 16:15:59 +0900

    components add

---
+++ vue-todo-list/src/components/TodoFooter.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>footer</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ vue-todo-list/src/components/TodoList.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>list</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
